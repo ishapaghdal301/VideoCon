d03180a246eacc26ea1f6b22cb0095b2ee4de7e6
MananPatel21 <21ceuos018@ddu.ac.in>
2024-02-17 11:52:15 +0530

    folder structure

---
+++ flask/src/models/user.py
@@ -0,0 +1,7 @@
+from mongoengine import Document, StringField
+
+class User(Document):
+    first_name = StringField(required=True)
+    last_name = StringField(required=True)
+    email = StringField(required=True, unique=True)
+    password = StringField(required=True)
--- flask/src/routes/routes.py
+++ flask/src/routes/routes.py
@@ -1,21 +1,27 @@
 from flask import jsonify, request, session
 from app import app, mongo
+from src.services.user_services import UserService
 
 @app.route('/api/register', methods=['POST'])
 def register():
     data = request.json
+    print(data)
     users = mongo.db.users
-    existing_user = users.find_one({'email': data['email']})
+    
+    first_name = data['first_name']
+    last_name = data['last_name']
+    email = data['email']
+    password = data['password']
+    
+    if not all([first_name, last_name, email, password]):
+        return jsonify({'error': 'Missing required fields'}), 400
+    
+    existing_user = UserService.get_user_by_email(email)
     if existing_user:
         return jsonify({'error': 'Email already exists'}), 400
 
-    users.insert_one({
-        'first_name': data['first_name'],
-        'last_name': data['last_name'],
-        'email': data['email'],
-        'password': data['password1']  # Consider hashing the password
-    })
-    return jsonify({'message': 'Registration successful'}), 200
+    user = UserService.create_user(first_name, last_name, email, password)
+    return jsonify({'message': 'Registration successful', 'user': user.to_json()}), 200
 
 @app.route('/api/login', methods=['POST'])
 def login():
