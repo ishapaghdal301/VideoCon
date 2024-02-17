0706730730c0fea6122e5580ccefed77d1392dd8
manthan161203 <manthanlp3204@gmail.com>
2024-02-17 10:35:37 +0530

    mongoDB atlas

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify, session, redirect, url_for
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-from werkzeug.security import check_password_hash
+# from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 CORS(app)
 
 app.config['SECRET_KEY'] = 'your_secret_key'  
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/VideoCo'  
+app.config['MONGO_URI'] = 'mongodb+srv://ISMM:ismm@interviewmap.obh2ylj.mongodb.net/interviewMap'  
 mongo = PyMongo(app)
 
 @app.route('/api/register', methods=['POST'])
