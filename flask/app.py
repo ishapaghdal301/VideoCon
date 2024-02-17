50d88545015d9757db46541925eff55a0463d295
MananPatel21 <21ceuos018@ddu.ac.in>
2024-02-17 11:25:16 +0530

    running

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,10 @@
-from app import app
+from flask import Flask, request, jsonify, session, redirect, url_for
+from flask_cors import CORS
+from flask_pymongo import PyMongo
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+CORS(app)
+
+app.config['SECRET_KEY'] = 'your_secret_key'  
+app.config['MONGO_URI'] = 'mongodb+srv://ISMM:ismm@interviewmap.obh2ylj.mongodb.net/interviewMap'  
+mongo = PyMongo(app)
--- flask/src/__init__.py
+++ flask/src/__init__.py
@@ -9,4 +9,4 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['MONGO_URI'] = 'mongodb+srv://ISMM:ismm@interviewmap.obh2ylj.mongodb.net/interviewMap'  
 mongo = PyMongo(app)
 
-from app import routes
+from flask.run import routes
