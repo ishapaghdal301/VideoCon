9d0292719c43a5bafee6a58c69294b6521f3e8fb
manthan161203 <manthanlp3204@gmail.com>
2024-02-23 14:56:12 +0530

    add api

--- backend/src/routes/problemRouter.js
+++ backend/src/routes/problemRouter.js
@@ -4,5 +4,6 @@ const problemController = require('../controllers/problemController');
 
 // Get a single problem by ID
 router.get('/:id', problemController.getProblemById);
+router.post('/submitproblem/:id', problemController.submitProblem);
 
 module.exports = router;
